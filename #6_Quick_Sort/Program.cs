80842915066674afe94b36fa8894f9454a98aea7
Vitaliy <vitaliy.klimov@mail.ru>
2023-01-16 23:12:39 +0300

    QuickSort added

--- #6_Quick_Sort/Program.cs
+++ #6_Quick_Sort/Program.cs
@@ -1,26 +1,20 @@
 ﻿using System;
-
 namespace ConsoleApp3
-
 /*
                               v - pivot
 1. arr = {1, 0, -6, 3, -1, 5, 2}
 2. pivot = arr[6] (опорный элемент) => [1, 0, -6, -1] 2 [3, 5]
 3. Вызвать шаги 1-2 для подмассива слева от pivot и справа от pivot
 */
-
-
 {
     partial class Program
     {
-        //0, 2, -5, 5, 3, 7, 9, -1
-        //0, 2, -5, 5, 3, 7, 9, -1
         static void Main(string[] args)
         {
             int[] arr = { 0, -5, 2, 3, 5, 9, -1, 7 };
             QuickSort(arr, 0, arr.Length - 1);
             Console.Write($"Отсортированный массив {string.Join(", ", arr)}");
         }
-        
+
     }
 }
